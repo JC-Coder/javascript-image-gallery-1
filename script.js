c91e0a7159ac769c8c51a2bde249744124b3498c
JC-Coder <josephchimezie2003@gmail.com>
2022-07-27 16:43:58 +0100

    added files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var form = document.getElementById('form');
+
+var parentDiv = document.getElementById('result');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var reader = new FileReader();
+    
+    var name = document.getElementById('image').files[0].name;
+
+    reader.addEventListener('load', function(){
+        if(this.result && localStorage){
+            window.localStorage.setItem(name, this.result);
+            alert("image stored in local storage");
+            parentDiv.innerHTML = '';
+            showImages();
+        } else {
+            alert("not successful ")
+        }
+    })
+
+    reader.readAsDataURL(document.getElementById('image').files[0]);
+
+
+})
+
+function showImages(){
+    //
+var savedImages = window.localStorage.getItem('images');
+if (savedImages) savedImagesObject = JSON.parse(savedImages);
+else savedImagesObject = [];
+//
+
+    for (let i = 0; i < window.localStorage.length; i++){
+        let res = window.localStorage.getItem(window.localStorage.key(i))
+        var image = new Image();
+        image.src = res;
+
+        parentDiv.appendChild(image);
+    }
+}
+
+function remove(){
+    window.localStorage.clear();
+    parentDiv.innerHTML = '';
+}
+
+showImages();
